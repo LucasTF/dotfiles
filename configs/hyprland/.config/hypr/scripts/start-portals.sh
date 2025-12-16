8069f2349154cd526b45d8b628a725d83c01ce6d
Lucas Ferreira <lucas.htferreira@outlook.com>
2025-12-16 19:49:07 -0300

    0.2.5: Removal of KDE file picker by default.

--- configs/hyprland/.config/hypr/scripts/start-portals.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sleep 2
-killall xdg-desktop-portal-hyprland xdg-desktop-portal-kde xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-hyprland &
-sleep 2
-/usr/lib/xdg-desktop-portal-kde &
-/usr/lib/xdg-desktop-portal &
