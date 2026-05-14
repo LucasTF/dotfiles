0f3a6239540280f7b91bf64d488ff29da78153db
Lucas Ferreira <lucas.htferreira@outlook.com>
2026-05-13 21:14:47 -0300

    0.6.0: Migrate Hyprland apps configuration to Lua.

---
+++ configs/hyprland/.config/hypr/hyprland/apps.lua
@@ -0,0 +1,10 @@
+return {
+    terminal = "kitty",
+    file_manager = "thunar",
+    browser = "brave",
+    menu = "rofi -show drun",
+    logout = "wlogout -b 3",
+    spotify = "~/.config/hypr/hyprland/scripts/spotify.sh",
+    discord = "~/.config/hypr/hyprland/scripts/discord.sh",
+    code = "~/.config/hypr/hyprland/scripts/code_editor.sh",
+}
