c3c538b3066ad4822ee33ca176016d36389fe327
Lucas Ferreira <lucas.htferreira@outlook.com>
2026-05-16 16:57:52 -0300

    0.6.0: Change basic theme.

--- configs/hyprland/.config/hypr/hyprland/theme.lua
+++ configs/hyprland/.config/hypr/hyprland/theme.lua
@@ -1,8 +1,8 @@
 local theme = {
-    primary_color = "b16286ee",
-    primary_color_s = "d3869bee",
-    secondary_color = "689d6aee",
-    secondary_color_s = "8ec07cee",
+    primary_color = "458588ee",
+    primary_color_s = "83a598ee",
+    secondary_color = "98971aee",
+    secondary_color_s = "b8bb26ee",
     border_inactive_color = "928374aa"
 }
 
